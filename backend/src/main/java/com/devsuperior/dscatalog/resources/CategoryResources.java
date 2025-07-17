a9f43e5b4eaa215b443b5ac19f04e9ad81b32f34
rafazim <rafael_zim@hotmail.com>
2025-07-17 10:12:56 -0300

    DELETE category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
@@ -41,4 +41,10 @@ public class CategoryResources {
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }

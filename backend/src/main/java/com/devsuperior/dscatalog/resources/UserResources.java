6b52f2e357b3dd6381a87f11000294051bb8ad72
rafazim <rafael_zim@hotmail.com>
2025-11-04 23:01:03 -0300

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/resources/UserResources.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResources.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.dto.UserDTO;
 import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,8 +41,8 @@ public class UserResources {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
-        dto = service.update(id, dto);
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+        UserDTO newDto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
 
